e4ca48a3df349d1a1680c0c180b9a5032cf87410
Byzic <lbyzova2001@yandex.ru>
2021-09-06 22:45:29 +0300

    Валидация значений

--- js/main.js
+++ js/main.js
@@ -1,11 +1,53 @@
 function validateX() {
-    if ($('.x.radio').is(':checked')) {
-        $('.xbox-label').removeClass('box-error');
-        return true;
-      } else {
-        $('.xbox-label').addClass('box-error');
-        return false;
-      }
+  if ($('.x-radio').is(':checked')) {
+      return true;
+  } else {
+    alert("Необходимо ввести значение X")
+    return false;
+  }
 }
+function validateY() {
+  const Y_MIN = -5;
+  const Y_MAX = 5;
+
+  let yField = $('#y-textfield');
+  let numY = yField.val().replace(',', '.');
+  
+  if (!(new RegExp("/[^0-9]||[^.]/").test(numY))){
+      alert("Значение Y не является числом");
+      return false;
+  }else{
+
+      if (numY >= Y_MIN && numY <= Y_MAX){
+        
+          return true;
+      }else{
+          alert("Значение Y не входит в интервал (-5,5)")
+          return false;
+      }
+  }
+  
+}
+function validateR() {
+  if ($('.r-checkbox').is(':checked')) {
+
+      return true;
+  } else {
+      alert("Значение R  не выбрано")
+      return false;
+  }
+}
+function validateForm() {
+  return validateX() & validateY() & validateR();
+}
+
 $('button').slideUp( 'slow');
 $('button').slideDown( 'slow');
+
+$('#main-form').on('submit', function(event) {
+  event.preventDefault();
+  if (!validateForm()) return;
+  else{
+    $("form").submit();
+  }
+})
